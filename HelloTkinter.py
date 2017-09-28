2d3b11f2febe5a905a50f5a2995b7ac9aeefb63a
Jack Pincombe <jackpincombe1@gmail.com>
2017-09-28 13:40:42 +0100

    Adding functions to buttons

--- HelloTkinter.py
+++ HelloTkinter.py
@@ -1,8 +1,35 @@
 from tkinter import *
 
+
+class Window(Frame):
+
+    def __init__(self,master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+
+        #Changing the name of the master widget
+        self.master.title("GUI")
+
+        #allowing the widget to take the full width of the root window
+        self.pack(fill=BOTH, expand=1)
+
+        #creating the button instance
+        quitButton = Button(self,text="Quit",command=self.client_exit)
+
+        #placing the button into the window
+        quitButton.place(x=100, y=100)
+
+    def client_exit(self):
+        exit()
+
 root = Tk()
 
-w = Label(root, text="Hello Tkinter!")
-w.pack()
+#The size of the window
+root.geometry("400x300")
+
+app = Window(root)
 
 root.mainloop()
